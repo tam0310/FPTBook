f47d0ca72a31c1b860833b7afe7f013d2a51ff94
admin <admin@DESKTOP-6QHDM1R>
2023-12-07 14:14:15 +0700

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FPT_Book.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
